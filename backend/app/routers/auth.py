83525395493d8e72b908256fe81abe9fd86e00cc
deepika-vadla <vadladeepika1203@gmail.com>
2026-08-06 13:53:44 +0530

    Restore auth router

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
+
 from backend.app.database import get_db
-from backend.app.schemas.auth import UserRegister, UserLogin
+from backend.app.schemas.auth import UserRegister
 from backend.app.models.user import User
 from backend.app.utils.security import hash_password, verify_password
 from backend.app.utils.jwt_handler import create_access_token
@@ -12,7 +13,6 @@ router = APIRouter()
 
 @router.post("/register")
 def register(user: UserRegister, db: Session = Depends(get_db)):
-
     hashed_password = hash_password(user.password)
 
     new_user = User(
@@ -29,12 +29,13 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
     return {
         "message": "User Registered Successfully"
     }
+
+
 @router.post("/login")
 def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
-
     db_user = db.query(User).filter(
         User.email == form_data.username
     ).first()
