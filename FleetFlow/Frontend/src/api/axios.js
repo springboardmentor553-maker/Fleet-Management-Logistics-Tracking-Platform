5bebebea26d4d71c2c7c16da9e7ccfa9d682c1b2
Dattatri Madakatte <madakattedattatri69@gmail.com>
2026-08-21 16:01:20 +0530

    Update frontend API URL

--- FleetFlow/Frontend/src/api/axios.js
+++ FleetFlow/Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://fleetflow-backend-pnu3.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
