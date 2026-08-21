645929fbad11358d2306d4d44489853c1fc92bb0
Ashritha05-Madishetty <madishettyashritha@gmail.com>
2026-08-21 21:17:03 +0530

    Fix CORS for localhost 5174

--- FleetManagementBackend/app/main.py
+++ FleetManagementBackend/app/main.py
@@ -66,7 +66,7 @@ def check_db():
 
 CORS_ORIGINS = os.getenv(
     "CORS_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173"
+    "http://localhost:5174,http://127.0.0.1:5174"
 )
 
 allowed_origins = [
